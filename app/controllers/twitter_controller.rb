4f8676bf480628b30b3eb7a3c4f36872ba8ebabe
sandeep <spatlemail@gmail.com>
2018-11-20 12:09:13 +0100

    Add search post method to call twitter search api and display tweets

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -3,4 +3,12 @@ class TwitterController < ApplicationController
   def index
     @tweets = []
   end
+
+  def search
+    @tweets = Twitter::Client.new.search({ q: params[:query] })
+
+    respond_to do |format|
+       format.js
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'twitter/index'
 
+  post 'twitter/search'
+
   root 'twitter#index'
 end
